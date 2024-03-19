a9f2e2d7806bb5684759b1f35b8100c1ec4cd61c
YaelChar417 <A01711111@tec.mx>
2024-03-19 16:11:41 -0600

    holaaaaaaaaaaaaa

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-//ESCRIBAN AQUI
-
-int main() {
-
-}
+def calcular_area_triangulo(base,altura):
+    area = (base*altura)/2
+    return area
+    
+def main():
+    base = float(input("Ingresa la base del triangulo:  "))
+    altura = float(input("Ingresa la altura del triangulo:  "))
+    res = calcular_area_triangulo(base,altura)
+    print(res)
+    
+main()
