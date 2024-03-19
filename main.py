d100b3e09bc5f041cb6640fa923a5677dc2911d2
Paulina Castellanos <paulina@MacBook-Air-de-Paulina-7.local>
2024-03-19 16:26:32 -0600

    cambio pau

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def resta():
-    print("Numero 1")
-    num1=int(input())
-    print("Numero 2")
-    num2=int(input())
-    resta=num1-num2
-    print("Resta:", resta)
+def suma(a,b):
+    s=a+b
+    print(s)
     
-resta()
+suma()
