2b4f46f815dfe4f2ca31dca307cf16eb8737fbd1
alexaGonLuc16 <a01277701@tec.mx>
2024-03-19 17:44:49 -0600

    Funcion suma

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
-def multiplicar(num1, num2):
-    resultado = num1 * num2
-    return resultado
+def suma(a, b):
+    return a+b
 
-num1 = 5
-num2 = 10
-resultado = multiplicar(num1, num2)
-print("El resultado de la multiplicación es:", resultado)
+a=2
+b=1
 
-# piramide
-
-def piramide(n):
-    for i in range(1, n+1):
-        print(" " * (n-i) + "*" * (2*i-1))
-
-piramide(5)
-
-def calcular_area_triangulo(base,altura):
-    area = (base*altura)/2
-    return area
-    
-def main():
-    base = float(input("Ingresa la base del triangulo:  "))
-    altura = float(input("Ingresa la altura del triangulo:  "))
-    res = calcular_area_triangulo(base,altura)
-    print(res)
-    
-main()
+print(suma(a,b))
