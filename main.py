a6bc72206e7a4ca6ba59a9e0b9c9d9c853b3a7ef
ulmo369 <a01366802@itesm.mx>
2024-03-19 15:56:53 -0600

    Emilio cambios

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-#include <bits/stdc++.h>
-using namespace std;
+def binarysearch():
+    # binary search
 
-//ESCRIBAN AQUI
+    # input
+    n = int(input("Enter the number of elements: "))
+    arr = list(map(int, input("Enter the elements: ").split()))
+    x = int(input("Enter the element to search: "))
+    arr.sort()
 
-int main() {
-
-}
+    # binary search
+    low = 0
+    high = n - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
