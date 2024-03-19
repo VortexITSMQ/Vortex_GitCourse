dbb33270a25cde479cebcc75acce65db7360088b
Paulina Castellanos <paulina@MacBook-Air-de-Paulina-7.local>
2024-03-19 15:58:13 -0600

    CambiosPaulina

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-//ESCRIBAN AQUI
-
-int main() {
-
-}
+def resta():
+    print("Numero 1")
+    num1=int(input())
+    print("Numero 2")
+    num2=int(input())
+    resta=num1-num2
+    print("Resta:", resta)
+    
+resta()
